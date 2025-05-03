d1355406e4f961440d05261489b85becbfbfd535
katoy <youichikato@gmail.com>
2025-05-04 02:49:24 +0900

    Change layout

--- todo/src/main.py
+++ todo/src/main.py
@@ -1,13 +1,28 @@
 import flet as ft
 
+
 def main(page: ft.Page):
     def add_clicked(e):
-        page.add(ft.Checkbox(label=new_task.value))
+        tasks_view.controls.append(ft.Checkbox(label=new_task.value))
         new_task.value = ""
-        page.update()
+        view.update()
 
-    new_task = ft.TextField(hint_text="What's needs to be done?")
+    new_task = ft.TextField(hint_text="What needs to be done?", expand=True)
+    tasks_view = ft.Column()
+    view=ft.Column(
+        width=600,
+        controls=[
+            ft.Row(
+                controls=[
+                    new_task,
+                    ft.FloatingActionButton(icon=ft.Icons.ADD, on_click=add_clicked),
+                ],
+            ),
+            tasks_view,
+        ],
+    )
 
-    page.add(new_task, ft.FloatingActionButton(icon=ft.Icons.ADD, on_click=add_clicked))
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.add(view)
 
 ft.app(main)
