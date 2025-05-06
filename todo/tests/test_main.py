5b963c5884c89f7decb5b5563054d6a541b36b97
katoy <youichikato@gmail.com>
2025-05-06 09:52:38 +0900

    ruff で整形

--- todo/tests/test_main.py
+++ todo/tests/test_main.py
@@ -10,7 +10,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from src.assets.translations import translations
 import src.main
 
-from src.main import Task, TodoApp, main, todo_app
+from src.main import Task, TodoApp
 
 
 class MockPage:
